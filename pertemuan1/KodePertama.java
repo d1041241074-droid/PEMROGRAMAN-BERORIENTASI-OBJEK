c69363f936f0fc810cabc7bf47fc06d57cade8bd
d1041241074-droid <d1041241074@student.untan.ac.id>
2026-02-05 21:07:43 +0700

    Tugas Pertemuan 1 PBO

---
+++ pertemuan1/KodePertama.java
@@ -0,0 +1,13 @@
+package pertemuan1;
+
+public class KodePertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Kamis, 05 Februari 2026
+*/
---
+++ pertemuan1/KodePertama_Latihan1.java
@@ -0,0 +1,15 @@
+package pertemuan1;
+
+public class KodePertama_Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("Nama               : Urai Salwa Kirania Nadia");
+        System.out.println("Nim                : D1041241074");
+        System.out.println("Semester           : 4");
+        System.out.println("Prodi              : Informatika");
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Kamis, 05 Februari 2026
+*/
