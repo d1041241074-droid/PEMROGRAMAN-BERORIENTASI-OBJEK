841fa8cfc59b5889c4b09557df61edff77ed1fd4
d1041241074-droid <d1041241074@student.untan.ac.id>
2026-02-13 07:26:44 +0700

    Latihan Pertemuan 3

---
+++ pertemuan3/p022WorkingVariable.java
@@ -0,0 +1,22 @@
+package pertemuan3;
+
+public class p022WorkingVariable {
+    public static void main(String[]args){
+        //Deklarasi dan inisialisasi variabel
+        int angka = 10;
+
+        //Deklarasi nilai awal variabel
+        System.out.println("Nilai awal angka: "+ angka);
+
+        //Mengubah nilai variabel
+        angka = 25;
+
+        //Menampilkan nilai setelah perubahan
+        System.out.println("Nilai angka setelah diubah: " + angka);
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Kamis, 12 Februari 2026
+*/
---
+++ pertemuan3/p023PrimitiveData.java
@@ -0,0 +1,31 @@
+package pertemuan3;
+public class p023PrimitiveData{
+    public static void main(String[]args){
+        //Tipe data bilangan bulat
+        int umur = 20;
+        long populasi = 8000000000L;
+
+        //Tipe data bilangan desimal
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+
+        //Tipe data karakter
+        char nilaiHuruf = 'A';
+
+        //Tipe data Logika
+        boolean lulus = true;
+
+        System.out.println("Umur                  : " + umur);
+        System.out.println("Populasi              : " + populasi);
+        System.out.println("Tinggi Badan          : " + tinggi);
+        System.out.println("IPK                   : " + ipk);
+        System.out.println("Nilai Akhir           : " + nilaiHuruf);
+        System.out.println("Lulus                 : " + lulus);
+        
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Kamis, 12 Februari 2026
+*/
