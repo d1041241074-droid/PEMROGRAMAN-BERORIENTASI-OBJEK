2a2036a9d96184e0ff6ed2bf5b5d9b286c8038b2
d1041241074-droid <d1041241074@student.untan.ac.id>
2026-03-13 23:51:47 +0700

    Menambahkan Tugas Mandiri Pertemuan 13

---
+++ pertemuan13/LatihanArrayNama.java
@@ -0,0 +1,17 @@
+package pertemuan13;
+
+public class LatihanArrayNama {
+    public static void main(String[] args) {
+        String[] nama = {"Andi", "Budi", "Citra", "Deni", "Eka"};
+
+        System.out.println("=== DAFTAR NAMA ===:");
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println((i+1) + ". " + nama[i]);
+        }
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Jumat, 13 Maret 2026
+*/
---
+++ pertemuan13/LatihanArrayNilai.java
@@ -0,0 +1,19 @@
+package pertemuan13;
+
+public class LatihanArrayNilai {
+    public static void main(String[] args) {
+        int[] nilai = {85, 90, 78, 92, 88};
+
+        System.out.println("=== DAFTAR NILAI ===");
+        for(int i = 0; i < nilai.length; i++) {
+            System.out.println("Nilai " + (i+1) + ": " + nilai[i]);
+        }
+        System.out.println();
+        System.out.println("Total nilai: " + nilai.length);
+    }
+}
+
+/*Nama: Urai Salwa Kirania Nadia
+Nim: D1041241074
+Tanggal: Jumat, 13 Maret 2026
+*/
